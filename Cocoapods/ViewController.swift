ccd338b73087ce9a9458344a8964f54e5569f485
Patrick Horlebein <Patrick.Horlebein@check24.de>
2017-07-11 15:55:08 +0200

    Initial Commit

---
+++ Cocoapods/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cocoapods
+//
+//  Created by Patrick Horlebein on 11.07.17.
+//  Copyright © 2017 piay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
