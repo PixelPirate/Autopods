385263f820c31d390255a5d05fb171dec02d0253
Patrick Horlebein <Patrick.Horlebein@check24.de>
2018-04-26 14:01:42 +0200

    Fix progress circle

--- Cocoapods/Utilities/ProcessProgress.swift
+++ Cocoapods/Utilities/ProcessProgress.swift
@@ -7,8 +7,8 @@ final class ProcessProgress: Progress {
 
     func funcy(_ x: Double) -> Double {
         switch x {
-        case 0...0.25:
-            let t = inverseLerp(from: 0, to: 0.25, value: x)
+        case ...0.25:
+            let t = inverseLerp(from: 0, to: 0.25, value: max(x, 0))
             return (0...0.35).at(t)
         case 0.25...0.375:
             let t = inverseLerp(from: 0.25, to: 0.375, value: x)
@@ -16,9 +16,9 @@ final class ProcessProgress: Progress {
         case 0.375...0.575:
             let t = inverseLerp(from: 0.375, to: 0.575, value: x)
             return (0.4...0.5).at(t)
-        case 0.575...1:
-            let t = inverseLerp(from: 0.575, to: 1, value: x)
-            return (0.5...0.99).at(t)
+        case 0.575...:
+            let t = inverseLerp(from: 0.575, to: 1, value: min(x, 1))
+            return (0.5...0.9).at(t)
         default: return x
         }
     }
@@ -27,7 +27,7 @@ final class ProcessProgress: Progress {
         self.process = process
         super.init()
 
-        let max = 7.0
+        let max = 10.0
         let start = CFAbsoluteTimeGetCurrent()
         let end = start + max
         timer = Timer.scheduledTimer(withTimeInterval: 0.016, repeats: true) { timer in
